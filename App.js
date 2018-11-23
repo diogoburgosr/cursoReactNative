aabe58aa435c7b0dd11356d4c35a3b7e909b8383
Diogo <diogo@email.com>
2018-11-23 08:37:27 -0200

    adicionei Reactotron

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import "./ReactotronConfig";
+import Reactotron from "reactotron-react-native";
+
+export default class App extends React.Component {
+  render() {
+    Reactotron.log("Testando a conexão com o Reactotron");
+    return (
+      <View style={styles.container}>
+        <Text>CoponeyMobil teste</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
---
+++ ReactotronConfig.js
@@ -0,0 +1,5 @@
+import Reactotron from "reactotron-react-native";
+
+Reactotron.configure({ port: 9090 })
+  .useReactNative()
+  .connect();
