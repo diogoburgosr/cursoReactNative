75fe494cb6a2a9c832a3c76d1e5c7ca57114c7dc
Diogo <diogo@email.com>
2018-11-28 10:26:07 -0200

    Criando o formulário de inclusão de poneis. P.75-78

--- src/components/AdicionarPoneyScreen.js
+++ src/components/AdicionarPoneyScreen.js
@@ -2,15 +2,32 @@ import { Text } from "native-base";
 import React from "react";
 import { connect } from "react-redux";
 
+import PropTypes from "prop-types";
+import MantemPoneyForm from "./MantemPoneyForm";
+
 class AdicionarPoneyScreen extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  handleAddPoney = poney => {
+    alert("Ponei adicionado: " + JSON.stringify(poney));
+    this.props.navigation.navigate("ListarPoneys");
+  };
+
   render() {
-    return <Text>Tela de Inclusão de Poneis</Text>;
+    return (
+      <MantemPoneyForm
+        navigation={this.props.navigation}
+        onSubmit={this.handleAddPoney}
+      />
+    );
   }
 }
+
+AdicionarPoneyScreen.propTypes = {
+  navigation: PropTypes.object
+};
 
 const mapStateToProps = () => ({});
 const mapDispatchToProps = () => ({});
